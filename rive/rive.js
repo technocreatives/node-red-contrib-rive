968ee780c7d5918014dc2b501a1d540941d72312
Oskar Hagberg <oskar.hagberg@gmail.com>
2016-10-05 20:07:09 +0200

    Made the rive instance persist over inputs so it can handle state

--- rive/rive.js
+++ rive/rive.js
@@ -7,26 +7,38 @@ module.exports = function(RED) {
     RED.nodes.createNode(this,n);
     var node = this;
     this.script = n.script;
+    this.bot = new RiveScript({utf8: true, debug: false});
+    this.bot.stream(this.script);
+    this.bot.sortReplies();
 
     this.on('input', function(msg) {
 
-      var script = node.script;
-      var bot = new RiveScript({utf8: true, debug: false});
-      // TODO: setUservars('localuser', ...) from node context
-      bot.stream(script);
-      bot.sortReplies();
-      var reply = bot.reply('localuser', msg.payload);
+      var context = (typeof msg.context === 'object' && msg.context !== null) ? msg.context : {};
+      msg.context = context;
+      context.user = context.user || 'localuser';
+      context.vars = context.vars || {};
+      
+      node.bot.setUservars(context.user, context.vars);
+      
+      var reply = node.bot.reply(context.user, msg.payload);
 
       if(reply.match(/^ERR:/)) {
         msg.payload = reply;
         node.send([null,msg]);
       } else {
-        // TODO: get updated user vars from bot ( getUservars('localuser') ) and put back in node context
-        // omit 'topic', '__initialmatch__', '__history__' and '__lastmatch__'
+        context.vars = node.bot.getUservars(context.user);
+        delete context.vars.__initialmatch__;
+        delete context.vars.__history__;
+        delete context.vars.__lastmatch__;
+        delete context.vars.topic;
         msg.payload = reply;
         node.send([msg,null]);
       }
 
+    });
+
+    this.on('close', function() {
+      // tidy up any state
     });
 
   }
